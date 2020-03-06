79c0d509a5bf250185fdf252d92a5b7d43da6ae0
dhirajb7 <bond.dhiraj@gmail.com>
2020-03-07 04:07:57 +0530

    File generation done

--- externalSortStringProject/externalSortString/src/userIntraction/DataFromUser.java
+++ externalSortStringProject/externalSortString/src/userIntraction/DataFromUser.java
@@ -7,8 +7,6 @@ import projectHealper.Helper;
 public class DataFromUser extends Helper{
 	
 	private int sizeOfFile;
-	private int startNumber;
-	private int endNumber;
 	
 	public int getSizeOfFile() {
 		return sizeOfFile;
@@ -17,33 +15,16 @@ public class DataFromUser extends Helper{
 	private void setSizeOfFile(int sizeOfFile) {
 		this.sizeOfFile = sizeOfFile;
 	}
-	public int getStartNumber() {
-		return startNumber;
-	}
-	private void setStartNumber(int startNumber) {
-		this.startNumber = startNumber;
-	}
-	public int getEndNumber() {
-		return endNumber;
-	}
-	private void setEndNumber(int endNumber) {
-		this.endNumber = endNumber;
-	}
+	
 	
 	/**
 	 * Very First Method to run for File Generation.
 	 */
 	public void setData() {
 		Scanner scan = new Scanner(System.in);
-		displayLog("Enter Low Number");
-		int startNumber = scan.nextInt();
-		displayLog("Enter High Number");
-		int endNumber = scan.nextInt();
-		displayLog("Enter Minimum File Size to be generated in MB");
+		displayLog("Enter File Size to be generated in MB");
 		int sizeFile = scan.nextInt();
 		scan.close();
-		setStartNumber(startNumber);
-		setEndNumber(endNumber);
 		setSizeOfFile(sizeFile);
 	}
 	
